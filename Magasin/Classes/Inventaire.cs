2b89e91965c0838f4c240138a8ebe4872274fd6f
2233299 <2233299@cegepoutaouais.qc.ca>
2026-09-11 12:42:08 -0400

    premier commit yay

---
+++ Magasin/Classes/Inventaire.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magasin.Classes
+{
+    internal class Inventaire
+    {
+        private int basePath;
+        private int fichier;
+
+        public int ListeItems
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AjoutItem()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AjoutItems()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Contains()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Exporter()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Magasin/Classes/Magasin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magasin.Classes
+{
+    public class Magasin
+    {
+        public int Compte
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Inventaire
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Utilisateurs
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AjoutUtilisateur()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Authentification()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetUtilisateur()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void magasin()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void VendreItem()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
