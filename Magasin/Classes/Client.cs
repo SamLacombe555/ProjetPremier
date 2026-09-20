ae900215e66931e6dc4046677db09a488c4530f1
Sovann <2472525@cegepoutaouais.qc.ca>
2026-09-20 16:37:20 -0400

    Début création de la classe Client.

--- Magasin/Classes/Client.cs
+++ Magasin/Classes/Client.cs
@@ -1,54 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Magasin.Classes
 {
     public class Client
     {
-        public int Compte
+        // Déclaration de paramètres.
+        public Compte Compte { get; set; }
+        public List<string> Inventaire { get; set; }
+        public List<string> Panier { get; set; }
+
+        // Déclaration du constructeur.
+        public Client(Compte compte, List<string> inventaire, List<string> panier)
         {
-            get => default;
-            set
+            Compte = compte;
+            Inventaire = inventaire;
+            Panier = panier;
+        }
+
+
+        // Déclaration de méthodes.
+        public void AchatItem(string itemAcheter)
+        {
+            /// Fonction qui enlève une item de la liste "panier" pour le mettre
+            /// dans la liste "inventaire".
+            foreach (var item in Panier)
             {
+                if (item == itemAcheter)
+                    Inventaire.Add(itemAcheter);
+                    Panier.Remove(itemAcheter);
+                    Console.WriteLine("Items acheter");
             }
         }
 
-        public int Inventaire
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        public int Panier
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        public void AchatItem()
-        {
-            throw new System.NotImplementedException();
-        }
 
         public void AcheterPanier()
         {
-            throw new System.NotImplementedException();
+            /// Fonction qui verse tous la liste "panier" dans la liste "inventaire".
+            foreach (var item in Panier)
+            {
+                Inventaire.Add(item);
+            }
+            foreach (var item in Inventaire)
+            {
+                Panier.Remove(item);
+            }
         }
 
         public void AfficherPanier()
         {
-            throw new System.NotImplementedException();
+            /// Fonction qui montre le contenu du "panier".
+            foreach (var item in Panier)
+            {
+                Console.WriteLine(item);
+            }
         }
 
-        public void AjouterAuPanier()
+        public void AjouterAuPanier(string ItemAjouter)
         {
-            throw new System.NotImplementedException();
+            Panier.Add(ItemAjouter);
         }
 
         public void MethodeClient()
